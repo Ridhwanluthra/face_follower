76083a8c2478acd0c1cc42fb7e1910d2461691b5
hsgaurav <gauravisstar18@gmail.com>
2018-04-10 01:03:29 +0530

    first commit

--- scripts/tracking.py
+++ scripts/tracking.py
@@ -12,20 +12,37 @@ from rospy.numpy_msg import numpy_msg
 
 
 class image_tracker:
+
     def __init__(self):
+        self.ok = none
+        self.bbox = none
+        # initialize all varialbles like this
         rospy.init_node('image_tracker', anonymous=True)
         rospy.loginfo("Tracker Started")
         
         self.bridge = CvBridge()
 
+        self.bbox = rospy.Subscriber("coordinates", numpy_msg(Floats), self.subr)
+
+
+        #Where to publish
+        self._output_image_topic = "~image_topic_output"
+        print rospy.has_param(self._output_image_topic)
+        if rospy.has_param(self._output_image_topic):
+            output_image_topic = rospy.get_param(self._output_image_topic)
+            self.tracker_pub = rospy.Publisher(output_image_topic,Image, queue_size=10)
+      
+          
+        #Where to subscribe
         self._input_image_topic = "~image_topic_input"
-        print rospy.has_param(self._input_image_topic)
+        # print rospy.has_param(self_input_image_topic)
         if rospy.has_param(self._input_image_topic):
             input_image_topic = rospy.get_param(self._input_image_topic)
             self.image_sub = rospy.Subscriber(input_image_topic, Image, self.callback)
 
+        
 
-    def callback(self,data):
+    def callback(self, data):
         tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
         tracker_type = tracker_types[2]
 
@@ -43,107 +60,43 @@ class image_tracker:
             tracker = cv2.TrackerGOTURN_create()
 
         try:
-            ok, frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            ok = frame
         except CvBridgeError as e:
             print(e)
 
-        # Read video
-        video = cv2.VideoCapture(1)
+        # if not ok:
+        #     print "Cannot read video file"
+        #     sys.exit()
+        # ok = tracker.init(frame, bbox)
 
-        # Exit if video not opened.
-        if not video.isOpened():
-            print "Could not open video"
+        if not self.ok:
             sys.exit()
+        # Start timer
+        timer = cv2.getTickCount()
+        # Update tracker
+        self.ok, self.bbox = tracker.update(frame)
 
-        if not ok:
-            print "Cannot read video file"
-            sys.exit()
+        if self.ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+        else :
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
-        bbox = rospy.Subscriber("floats", numpy_msg(Floats), callback)
-        ok = tracker.init(frame, bbox)
+        try:
+            self.tracker_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
 
-        while True:
-            # Read a new frame
-            ok, frame = video.read()
-            if not ok:
-                break
-            # Start timer
-            timer = cv2.getTickCount()
-            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
 
-            if ok:
-                # Tracking success
-                p1 = (int(bbox[0]), int(bbox[1]))
-                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-            else :
-                # Tracking failure
-                cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-            # Display tracker type on frame
-            cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-        
-            # Display FPS on frame
-            cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        def subr(self, data):
+            print rospy.get_name(), "I heard %s"%str(data.data)\
+            self.ok = tracker.init(frame, bbox)
 
-            # Display result
-            cv2.imshow("Tracking", frame)
 
-    # # # Read video
-    # # video = cv2.VideoCapture(1)
-
-    # # # Exit if video not opened.
-    # # if not video.isOpened():
-    # #     print "Could not open video"
-    # #     sys.exit()
-
-    # # # Read first frame.
-    # # ok, frame = video.read()
-    # # if not ok:
-    # #     print 'Cannot read video file'
-    # #     sys.exit()
-    
-    # # Define an initial bounding box
-    # # bbox = (287, 23, 86, 320)
-
-    # # Uncomment the line below to select a different bounding box
-    # bbox = cv2.selectROI(frame, False)
-
-    # # Initialize tracker with first frame and bounding box
-    # ok = tracker.init(frame, bbox)
-
-    # while True:
-    #     # Read a new frame
-    #     ok, frame = video.read()
-    #     if not ok:
-    #         break
-        
-    #     # Start timer
-    #     timer = cv2.getTickCount()
-
-    #     # Update tracker
-    #     ok, bbox = tracker.update(frame)
-
-    #     # Calculate Frames per second (FPS)
-    #     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-
-    #     # Draw bounding box
-    #     if ok:
-    #         # Tracking success
-    #         p1 = (int(bbox[0]), int(bbox[1]))
-    #         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-    #         cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-    #     else :
-    #         # Tracking failure
-    #         cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-
-    #     # Display tracker type on frame
-    #     cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-    
-    #     # Display FPS on frame
-    #     cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-
-    #     # Display result
-    #     cv2.imshow("Tracking", frame)
 
 
 if __name__ == '__main__' :
@@ -156,6 +109,3 @@ if __name__ == '__main__' :
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
-    k = cv2.waitKey(1) & 0xff
-    if k == 27:
-        sys.exit()
