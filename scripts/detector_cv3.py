06e19a9142693cd91ce9514fdd8036acd4e3c30a
Gaurav <32589162+HsGaurav@users.noreply.github.com>
2018-04-22 17:59:21 +0530

    face detector file (#1)
    * publishing the coordinates of bounding box
    
    * publish improved
    
    * final publishing of bounding box errors fixed
    
    * minor changes
    
    * minor changes
    
    * first commit tracker to ros
    
    * first commit
    
    * Tracker with message filters
    
    * first commit modified to ros
    
    * added node of tracker and bounding boxes
    
    * added param and error floats does not support indexing
    
    * added param and error floats does not support indexing
    
    * added param
    
    * light changes
    
    * working with errors solved

--- scripts/detector_cv3.py
+++ scripts/detector_cv3.py
@@ -2,23 +2,21 @@
 import rospy
 import cv2
 import sys
+import logging as log
+import datetime as dt
+import numpy
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-
-
-import logging as log
-import datetime as dt
-
+from rospy_tutorials.msg import Floats
 from time import sleep
-
 
 class image_converter:
 
     def __init__(self):
+
         rospy.init_node('image_converter', anonymous=True)
-        
         rospy.loginfo("recognizer started")
         print "1................................................"
 
@@ -29,8 +27,14 @@ class image_converter:
         else:
             rospy.logwarn("parameters need to be set to start recognizer.")
             return
-        
-        
+
+        self._coordinates = "~coordinates"
+        print rospy.has_param(self._coordinates)
+        if rospy.has_param(self._coordinates):
+            coordinates = rospy.get_param(self._coordinates)
+            self.publ = rospy.Publisher(coordinates, Floats, queue_size=10)
+        # self.publ = rospy.Publisher('coordinates', Floats, queue_size = 10)
+
         self.bridge = CvBridge()
         self.faceCascade = cv2.CascadeClassifier(cascPath)
         
@@ -41,7 +45,6 @@ class image_converter:
             output_image_topic = rospy.get_param(self._output_image_topic)
             self.image_pub = rospy.Publisher(output_image_topic,Image, queue_size=10)
       
-          
         #Where to subscribe
         self._input_image_topic = "~image_topic_input"
         print rospy.has_param(self._input_image_topic)
@@ -49,19 +52,14 @@ class image_converter:
             input_image_topic = rospy.get_param(self._input_image_topic)
             self.image_sub = rospy.Subscriber(input_image_topic, Image, self.callback)
 
-    
-    
-    
 
     def callback(self,data):
+
         try:
             frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        #(rows,cols,channels) = cv_image.shape
-        #if cols > 60 and rows > 60 :
-          #cv2.circle(cv_image, (50,50), 10, 255)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = self.faceCascade.detectMultiScale(
@@ -70,18 +68,18 @@ class image_converter:
             minNeighbors=5,
             minSize=(30, 30)
         )
-
+        
         if len(faces):
             rospy.loginfo("I see a face %d",len(faces))
             rospy.logdebug("Partial: %s" + str(faces))
 
             # Draw a rectangle around the faces
             for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.circle(frame, (x+(w/2), y+(h/2)), 5, 255,-1)
-        
-            cv2.imshow("Image window", frame)
-            cv2.waitKey(3)
+                var = numpy.array([x, y, w, h], dtype=numpy.float32)
+                self.publ.publish(var)
+                
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.circle(frame, (x+(w/2), y+(h/2)), 5, 255,-1)
 
         try:
           self.image_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
@@ -92,7 +90,9 @@ class image_converter:
 if __name__ == '__main__':
     rospy.loginfo("simple_face_detection ...........")
     print "................................................"
+
     ic = image_converter()
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
--- scripts/tracking.py
+++ scripts/tracking.py
@@ -2,86 +2,94 @@
 import rospy
 import cv2
 import sys
+import logging as log
+import numpy
+import message_filters
 
-if __name__ == '__main__' :
+from rospy_tutorials.msg import Floats
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError   
+from message_filters import TimeSynchronizer, Subscriber
 
-    # Set up tracker.
-    # Instead of MIL, you can also use
+class image_tracker:
 
-    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-    tracker_type = tracker_types[2]
-
-    
-    if tracker_type == 'BOOSTING':
-        tracker = cv2.TrackerBoosting_create()
-    if tracker_type == 'MIL':
-        tracker = cv2.TrackerMIL_create()
-    if tracker_type == 'KCF':
-        tracker = cv2.TrackerKCF_create()
-    if tracker_type == 'TLD':
-        tracker = cv2.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW':
-        tracker = cv2.TrackerMedianFlow_create()
-    if tracker_type == 'GOTURN':
-        tracker = cv2.TrackerGOTURN_create()
-
-    # Read video
-    video = cv2.VideoCapture(1)
-
-    # Exit if video not opened.
-    if not video.isOpened():
-        print "Could not open video"
-        sys.exit()
-
-    # Read first frame.
-    ok, frame = video.read()
-    if not ok:
-        print 'Cannot read video file'
-        sys.exit()
-    
-    # Define an initial bounding box
-    bbox = (287, 23, 86, 320)
-
-    # Uncomment the line below to select a different bounding box
-    bbox = cv2.selectROI(frame, False)
-
-    # Initialize tracker with first frame and bounding box
-    ok = tracker.init(frame, bbox)
-
-    while True:
-        # Read a new frame
-        ok, frame = video.read()
-        if not ok:
-            break
+    def __init__(self):
+        rospy.init_node('image_tracker', anonymous=True)
+        rospy.loginfo("Tracker Started")
         
-        # Start timer
+        self.bridge = CvBridge()
+ 
+        #Where to publish
+        self._output_image_topic = "~image_topic_output"
+        print rospy.has_param(self._output_image_topic)
+        if rospy.has_param(self._output_image_topic):
+            output_image_topic = rospy.get_param(self._output_image_topic)
+            self.tracker_pub = rospy.Publisher(output_image_topic,Image, queue_size=10)
+        
+        #Where to subscribe
+        self._input_image_topic = "~image_topic_input"
+        print rospy.has_param(self._input_image_topic)
+        if rospy.has_param(self._input_image_topic):
+            input_image_topic = rospy.get_param(self._input_image_topic)
+            self.image_sub = message_filters.Subscriber(input_image_topic, Image)
+            
+        self._coordinates = "~coordinates"
+        print rospy.has_param(self._coordinates)
+        if rospy.has_param(self._coordinates):
+            coordinates = rospy.get_param(self._coordinates)
+            self.bbox = message_filters.Subscriber(coordinates, Floats)
+        
+        ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.bbox], 10, 10, allow_headerless=True)
+        ts.registerCallback(self.mycallback)
+
+    def mycallback(self, input_image_topic, coordinates):
+        tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
+        tracker_type = tracker_types[2]
+        # if tracker_type == 'BOOSTING':
+        #     tracker = cv2.TrackerBoosting_create()
+        # if tracker_type == 'MIL':
+        #     tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        # if tracker_type == 'TLD':
+        #     tracker = cv2.TrackerTLD_create()
+        # if tracker_type == 'MEDIANFLOW':
+        #     tracker = cv2.TrackerMedianFlow_create()
+        # if tracker_type == 'GOTURN':
+        #     tracker = cv2.TrackerGOTURN_create()
+        data = input_image_topic
+        bbox = coordinates.data
+        # rospy.loginfo(bbox)
+        try:
+            frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            ok = frame
+        except CvBridgeError as e:
+            print(e)
+
+        ok = tracker.init(frame, bbox)
+
         timer = cv2.getTickCount()
+        ok, a = tracker.update(frame)
 
-        # Update tracker
-        ok, bbox = tracker.update(frame)
-
-        # Calculate Frames per second (FPS)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-
-        # Draw bounding box
         if ok:
             # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
         else :
-            # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+            pass
 
-        # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-    
-        # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        try:
+            self.tracker_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
 
-        # Display result
-        cv2.imshow("Tracking", frame)
+if __name__ == '__main__' :
+    rospy.loginfo("simple face tracker...")
+    it = image_tracker()
 
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == 27 : break
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
