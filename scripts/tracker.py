27aae4a47d5e5f10d75b37cc4c0bc1d0bea0d4d6
ridhwanluthra <ridhwancode@gmail.com>
2018-05-04 04:22:28 +0530

    fixing jitter issues in tracker due to using last frame. reinitialising the tracker now works better. renamed files

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -21,20 +21,7 @@ class image_tracker:
         self.last_bbox = []
         self.last_frame = []
         self.tracking = False
-        tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-        tracker_type = tracker_types[2]
-        # if tracker_type == 'BOOSTING':
-        #     tracker = cv2.TrackerBoosting_create()
-        # if tracker_type == 'MIL':
-        #     tracker = cv2.TrackerMIL_create()
-        if tracker_type == 'KCF':
-            self.tracker = cv2.TrackerKCF_create()
-        # if tracker_type == 'TLD':
-        #     tracker = cv2.TrackerTLD_create()
-        # if tracker_type == 'MEDIANFLOW':
-        #     tracker = cv2.TrackerMedianFlow_create()
-        # if tracker_type == 'GOTURN':
-        #     tracker = cv2.TrackerGOTURN_create()
+        self.tracker = cv2.TrackerKCF_create()
  
         #Where to publish
         self._output_image_topic = "~image_topic_output"
@@ -56,7 +43,7 @@ class image_tracker:
             coordinates = rospy.get_param(self._coordinates)
             self.bbox = message_filters.Subscriber(coordinates, rlist)
         
-        ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.bbox], 10, 10, allow_headerless=True)
+        ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.bbox], 10, 10, allow_headerless=False)
         ts.registerCallback(self.mycallback)
 
 
@@ -67,9 +54,11 @@ class image_tracker:
             print(e)
 
         bbox = coordinates.data
+        # TODO: add functionality to take the last stored bbox and
+        #       continue tracking.
         if bbox:
             self.last_bbox = bbox
-            self.last_frame = frame
+            # self.last_frame = frame
 
         if not self.last_bbox:
             rospy.loginfo('had not recieved any face data yet')
@@ -80,14 +69,17 @@ class image_tracker:
             ok, bbox = self.tracker.update(frame)
             if not ok:
                 self.tracking = False
-                bbox = self.last_bbox
+                # bbox = self.last_bbox
         else:
-            # self.tracker.clear()
-            # self.tracker = cv2.TrackerKCF_create()
-            ok = self.tracker.init(frame, tuple(self.last_bbox))
-            self.tracking = True
-            bbox = self.last_bbox
-            frame = self.last_frame
+            self.tracker.clear()
+            self.tracker = cv2.TrackerKCF_create()
+            if bbox:
+                ok = self.tracker.init(frame, tuple(bbox))
+                self.tracking = True
+            else:
+                ok = False
+            # bbox = self.last_bbox
+            # frame = self.last_frame
 
         if ok:
             p1 = (int(bbox[0]), int(bbox[1]))
